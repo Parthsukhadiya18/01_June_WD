c714ce13f16eede7a162b59530b76f17c11656e5
Parth <Parthsir18@gmail.com>
2023-10-16 11:15:35 +0530

    first time

---
+++ jsMonika/03_if.js
@@ -0,0 +1,37 @@
+let a1 = 11;
+
+// if(a1<10){
+//     console.log("Hello Parth"+a1);
+// }
+// if(a1<=10){
+//     console.log("Hello Parth"+a1);
+// }else{
+//     console.log("Hello Earth"+a1);
+// }
+
+// if(a1<10){
+//     console.log("Hello Parth"+a1);
+// }else if(a1>10){
+//     console.log("Hello Rarth"+a1);
+// }
+// else{
+//     console.log("Hello Earth"+a1);
+// }
+
+
+let parth = 3;
+switch (parth) {
+    case 1:
+        console.log("Parth"+"Patel");
+        break;
+        case 2:
+            console.log("Monika"+"Patel");
+            break;
+            case 3:
+                console.log("Nency"+"Patel");
+                break;
+
+    default:
+        console.log("selected corrct opteon");
+        break;
+}
---
+++ jsMonika/03_oprater.js
@@ -0,0 +1,60 @@
+// opretor arthmetic 
+// + - * / % 
+// let a = 5;
+// let  b = 3;
+// let  Sum = a+b;
+// // document.write(sum);
+// console.log(Sum);
+// let   Sub= a-b;
+// console.log(Sub);
+// let   Mul= a*b;
+// console.log(Mul);
+// let   Div= a/b;
+// console.log(Div);
+// let   Muld= a%b;
+// console.log(Muld);
+
+
+// oprator Assin opreter += -= *= /=
+
+let a1 = 10;
+// a1 = a1+1;
+// a1+=1
+// a1-=1
+// a1*=4
+// a1/=2
+// console.log("value of a" +"="+ a1);
+
+
+// oprator relation opartor  < > <= >= == ===
+// let a2 = 10;
+// let a3 = '10';
+// console.log(a2<a3);
+// console.log(a2>a3);
+// console.log(a2==a3);
+// console.log(a2===a3);
+
+// logical oprator &&and  ||or  !not 
+let b1 = 10;
+let b2  = 12;
+let b3 = 15;
+
+// console.log(b1<b2 && b2<b3);
+// console.log(b1<b2 && b2>b3);
+// console.log(b1>b2 && b2<b3);
+// console.log(b1>b2 && b2>b3);
+
+// // or 
+console.log("or  exmaple")
+// console.log(b1<b2 || b2<b3);
+// console.log(b1<b2 || b2>b3);
+// console.log(b1>b2 || b2<b3);
+// console.log(b1>b2 || b2>b3);
+// // not 
+// console.log(!(b1>b2 || b2>b3));
+console.log(!(b1<b2));
+
+
+
+
+
